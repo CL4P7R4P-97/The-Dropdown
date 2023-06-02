586641a051e6c21e5e874756d8817b57c59ebc16
thecodeminerX <aky5271@gmail.com>
2023-06-02 11:48:17 +0530

    BetterVer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+// Import necessary dependencies
 import React from "react";
 import "./App.css";
 import Dropdown from "./Dropdown";
 
+// Define App component
 function App() {
   return (
+    // Render App component
     <div className="App">
-     <Dropdown />
+      <Dropdown />
     </div>
   );
 }
 
+// Export App component as default
 export default App;
--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,29 +1,51 @@
-import React from "react";
-import "./dropdown.css";
+import React, { useEffect, useState } from 'react';
+import './dropdown.css';
 
-function Dropdown() {
+const Dropdown = () => {
+
+  // State variables to control the state of the component
+const [isOpen, setIsOpen] = useState(false); // boolean value to check if dropdown is open or closed
+const [selectedOption, setSelectedOption] = useState(''); // string value to hold the selected option from dropdown
+
+// Array of choices for the dropdown
+const choices = ['Yes', 'No', 'Ask President', 'Ask Dad', 'Ask Girlfriend'];
 
 
-    const choices = ['Yes', 'No', 'Ask president', 'Ask instructor'];
+// Set default selected option to 'Select' when component mounts
+useEffect(() => {
+  setSelectedOption('Select');
+}, []);
 
-  return (
-   <div className="drop-container">
-  <h1>Should you use dropdown?</h1>
-  <div class="dropdown">
-  <button class="dropbtn">Select</button>
-  <div class="dropdown-content">
-   {
-    choices.map((choice)=>{
+ // Function to handle hover event
+const handleHover = () => {
+  setIsOpen(true); // Set isOpen state to true
+};
 
-        return ( <a href="#">{choice}</a>)
-    })
-   }
-     
+// Function to handle click event with option parameter
+const handleClick = (option) => {
+  setSelectedOption(option); // Set selectedOption state with the passed option parameter
+  setIsOpen(false); // Set isOpen state to false
+};
+  
+  // This code renders a dropdown component with a button and a list of choices. 
+// The selected option is displayed on the button, and when clicked, the list of choices is displayed. 
+// The user can then select a choice from the list, which updates the selected option.
+
+return (
+  <div className='drop-container'>
+    <h1>Should you use a dropdown?</h1>
+    <div className="dropdown" onMouseEnter={handleHover} onMouseLeave={() => setIsOpen(false)}>
+      <button className="dropbtn">{selectedOption} <span>&ensp;&darr;</span></button>
+      {isOpen && (
+        <div className="dropdown-content">
+          {choices.map((choice) => (
+            <a onClick={() => handleClick(choice)}>{choice}</a>
+          ))}
+        </div>
+      )}
+    </div>
   </div>
-</div>
-
-   </div>
-  );
-}
+);
+};
 
 export default Dropdown;
